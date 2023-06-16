d22816712a43bb786b18c41161f5873d67601b8d
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-06-16 14:02:23 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Profile } from './Profile/profile';
+
+import user from 'data/user.json';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <>
+   <Profile
+    location={user.location}
+    tag={user.tag}
+    username={user.username}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+  </>
 };
